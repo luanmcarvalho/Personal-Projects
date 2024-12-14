11353db2fbbc1d91adf940614a2c89ccbe81a8d5
luanmcarvalho <luanmcarvalho@hotmail.com>
2024-12-14 13:45:13 -0300

    clear

--- GENERAL/python/002_gender_register.py
+++
@@ -1,4 +0,0 @@
-x = str(input("Choose a gender M/F: ")).strip()
-while x not in 'mMfF':
-    x = str(input(("Not an actual option, please try again: "))).strip()
-print("Gender {}, saved!".format(x))
--- README.md
+++
@@ -1,2 +0,0 @@
-# PersonalProjects
-It's a collection of studies I do day to day.
